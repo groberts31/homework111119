71ac4bb4dbb8db93fef27097b02762ef2e7fbc9f
Greg Roberts <groberts3104@gmail.com>
2019-11-08 21:45:36 -0500

    added info for index.html, css.style, script.js and questions.js

---
+++ questions.js
@@ -0,0 +1,40 @@
+// Questions for Quiz
+
+var questions = [
+    {
+        quizQuestions: "Where do you add the stylesheets in HTML?",
+        
+        options: ["body", "head", "In The Script", "You Dont Add Them"],
+        
+        correctAnswer: "head"
+    },
+    {
+        quizQuestions: "What is the correct sequence of HTML tags for starting a webpage?",
+        
+        options: ["HTML, Head, Title", "Head, Title, HTML", "Title, Head, HTML", "HTML, Script, Head"],
+        
+        correctAnswer: "HTML, Head, Title"
+    },
+    {
+        quizQuestions: "Choose the correct HTML rtag for the largest heading",
+        
+        options: ["h5", "head", "heading", "h1"],
+        
+        correctAnswer: "h1"
+    },
+    {
+        quizQuestions: "How can you make a list that lists the items with numbers?",
+        
+        options: ["ol", "dl", "ul", "list"],
+        
+        correctAnswer: "ol"
+    },
+    {
+        quizQuestions: "HTML tags are surrounded by _________ brackets",
+        
+        options: ["Curly", "Round", "Angle", "Square"],
+        
+        correctAnswer: "Angle"
+    }
+
+];
---
+++ script.js
@@ -0,0 +1,75 @@
+// JavaScript vars and functions listed below
+
+var startBtnEl = document.querySelector("#startBtn");
+var quizQuestionsEl = document.querySelector("#qQues");
+var possAnsEl = document.querySelector("#possAnsBtns");
+var resultEl = document.querySelector("#result");
+var scoreEl = 0;
+var answersEl;
+
+
+document.getElementById("startBtn").onclick = function (myQuiz) {
+    disableButton();
+    renderMyQuiz();
+}
+
+var Counter = 0;
+var myQues = questions.length - 1;
+
+function renderMyQuiz() {
+    if (Counter <= myQues) {
+        var currentQ = questions[Counter].quizQuestions;
+        quizQuestionsEl.innerHTML = currentQ;
+        
+
+        var btn1 = document.getElementById("btn1");
+        btn1.textContent = questions[Counter].options[0];
+        btn1.style.display = "inline";
+        possAnsEl.appendChild(btn1);
+
+        var btn2 = document.getElementById("btn2");
+        btn2.textContent = questions[Counter].options[1];
+        btn2.style.display = "inline";
+        possAnsEl.appendChild(btn2);
+
+        var btn3 = document.getElementById("btn3");
+        btn3.textContent = questions[Counter].options[2];
+        btn3.style.display = "inline";
+        possAnsEl.appendChild(btn3);
+
+        var btn4 = document.getElementById("btn4");
+        btn4.textContent = questions[Counter].options[3];
+        btn4.style.display = "inline";
+        possAnsEl.appendChild(btn4);
+    }
+    else {
+        ResultsFun();
+    }
+}
+possAnsEl.addEventListener("click", function (event) {
+    if (event.target.textContent == questions[Counter].correctAnswer) {
+        alert("You're Correct!!!");
+        scoreEl++;
+    }
+    else {
+        alert("Not this time!!!");
+    }
+    Counter++;
+    renderMyQuiz();
+})
+function disableButton() {
+    document.getElementById("quizGo").style.display = "none";
+}
+function ResultsFun() {
+    possAnsEl.style.display = "none";
+    quizQuestionsEl.style.display = "none";
+    var showScore = document.getElementById("score");
+    var txt = document.getElementById("result");
+    txt.textContent = "Anddd Done!!! Check Your Score Out."
+    showScore.style.display = "block";
+    showScore.textContent = "Calculate Score";
+    resultEl.appendChild(showScore);
+}
+document.getElementById("score").onclick = function (myQuiz) {
+    alert(scoreEl);
+}
